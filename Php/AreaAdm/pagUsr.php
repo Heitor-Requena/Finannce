bab287e27bd4bdbaef20a31d46b2a21d1896951c
João Vítor <140274816+VitorAlvs@users.noreply.github.com>
2023-11-07 20:27:08 -0300

    Update pagUsr.php

--- Php/AreaAdm/pagUsr.php
+++ Php/AreaAdm/pagUsr.php
@@ -1,33 +1,41 @@
 <!DOCTYPE html>
 <html lang="en" data-bs-theme="dark">
-
+<!-- Declaração de tema dark -->
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="..." crossorigin="anonymous">
+  <!-- Inclusão do CSS do Bootstrap -->
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+  <!-- Inclusão do arquivo de ícones do Bootstrap -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="..." crossorigin="anonymous"></script>
+  <!-- Inclusão do JavaScript do Bootstrap -->
   <link rel="stylesheet" href="../../Css/Acessos/Adm/indexAdm.css">
-
   <title>Adm</title>
 </head>
-
 <body>
   <?php
   session_start();
+  // Inicia a sessão
 
   if ((!isset($_SESSION["id"]) == true) and (!isset($_SESSION["nome"]) == true) and (!isset($_SESSION["email"]) == true)) {
+    // Se as variáveis de sessão não estiverem definidas
     unset($_SESSION["id"]);
     unset($_SESSION["id"]);
     unset($_SESSION["id"]);
+    // Remove essas variáveis de sessão 
     header('location: ../../index.html');
+    // Redireciona para o arquivo index.html
   } else {
     $Nome = $_SESSION["nome"];
+    // Atribui o valor da variável de sessão "nome" à variável local $Nome
   }
   ?>
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+  <!-- Estrutura da barra de navegação -->
+  <nav class="navbar navbar-expand-lg bg-body-tertiary fixed-top">
+    <!-- Barra de navegação -->
     <div class="container-fluid ml-2">
       <a class="navbar-brand" href="#" id="alterarLogo"><img src="../../Img/Logo/logo branca finannce.png" alt="" style="height: 56px;"></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
@@ -66,28 +74,34 @@
       </div>
     </div>
   </nav>
-  
+
+  <!-- Botões de ação na interface -->
   <div class="nav2-align text-center">
+    <!-- Div com classe de alinhamento -->
     <div class="btn-group">
+      <!-- Botões de Ação -->
       <a href="#" class="btn btn-outline-light" id="btn_cons"  onclick="alterarPag('ConsUsr')">Consultar</a>
       <a href="#" class="btn btn-outline-light" id="btn_altlog"   onclick="alterarPag('AltUsr')">Alterar Login</a>
       <a href="#" class="btn btn-outline-light" id="btn_del"    onclick="alterarPag('DelUsr')">Deletar</a>
     </div>
-    
   </div>
+
   <section id="section" style="margin-top: 100px;">
+  
     <h1 class="text-center align-middle mt-5"></h1>
   </section>
 
-
   <section id="result"></section>
+  <!-- Seção para resultados -->
 
+  <!-- Inclusão do jQuery -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <!-- Inclusão de scripts locais -->
   <script src="../../Js/AreaAdm/AltPag.js"></script>
   <script src="../../Js/AreaAdm/RespostaUsr-Adm.js"></script>
   <script src="../../Js/AreaAdm/RespostaCon-Adm.js"></script>
   <script src="../../Js/AreaAdm/RespostaAdm-Adm.js"></script>
   <script src="../../Js/AreaAdm/RespostaArtigo-Adm.js"></script>
   <script src="../../Js/AreaAdm/RespostaFAQ-Adm.js"></script>
-
+</body>
 </html>
